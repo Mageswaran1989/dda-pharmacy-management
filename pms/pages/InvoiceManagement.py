4d17144893268e759c04be43e225dbdf9eee4f34
Pawan Prajapati <pawan.prajapati@rapyuta-robotics.com>
2022-11-02 09:37:51 +0530

    Invoid Mgt

--- pms/pages/InvoiceManagement.py
+++ pms/pages/InvoiceManagement.py
@@ -1,14 +1,20 @@
 import streamlit as st
 from db_utils.sqlalchemy_backend import execute, read_sql_query_as_df
 from streamlit_option_menu import option_menu
+from utils import display_table, handle_table_deletes, multiselect_options, select_options
 
 # conn = init_db_connection()
 
 st.title("InvoiceManagement")
 
 
-view_tab, add_tab, delete_tab = st.tabs(["View", "Add", "Delete"])
+view_tab,  = st.tabs(["View", ])
 
-
-
-
+with view_tab:
+    from_date = st.date_input("From date", key="date")
+    to_date = st.date_input("To date", key="date")
+    display_table(query=f"SELECT * FROM PurchaseOrder WHERE date BETWEEN '{from_date}' and '{to_date}'")
+    df = read_sql_query_as_df(f"SELECT SUM(cost) FROM PurchaseOrder WHERE date BETWEEN '{from_date}' and '{to_date}'")
+    total_sales = df["sum"][0]
+    sales_string = "Total sales for the period is " + str(total_sales)
+    st.subheader(sales_string)    
